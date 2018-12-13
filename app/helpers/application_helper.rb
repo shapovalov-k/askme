aedcb1f281c4ab12c7e27211528ab047106da028
shapovalov-k <shapovalovkv@gmail.com>
2018-12-13 21:00:58 +0100

    add number of questions with declension

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       asset_path 'no_avatar.png'
     end
   end
+
+  def declension(number, vopros, voprosa, voprosov)
+    remainder_10 = number.to_i % 10
+    remainder_100 = number.to_i % 100
+
+    if ((number.between?(10,14)) || ((remainder_100).between?(10,14)))
+      return voprosov
+    end
+    return vopros if remainder_10 == 1
+    return voprosa if remainder_10.between?(1,4)
+    return voprosov if (remainder_10.between?(5,9) || remainder_10 == 0)
+  end
 end
