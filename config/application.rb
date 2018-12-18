9827632fb2cee3ec8bf6a615b5197037483a7158
shapovalov-k <shapovalovkv@gmail.com>
2018-12-18 22:06:17 +0100

    add Questions controller

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,17 @@ module Askme
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework  nil #to skip test framework
+    end
+    config.time_zone = 'Moscow'
+
+    config.i18n.default_locale = :en
+    config.i18n.locale = :ru
+
+    config.i18n.fallbacks = [:en]
   end
 end
